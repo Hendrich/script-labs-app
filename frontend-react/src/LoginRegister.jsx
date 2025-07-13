d4f9c94126df3fb6843213a15918f35d70aa031c
Hendri Christianto <95005976+hendrich2404@users.noreply.github.com>
2025-07-13 17:16:46 +0700

    fix:update url di file loginRegister.js

--- frontend-react/src/LoginRegister.jsx
+++ frontend-react/src/LoginRegister.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 function LoginRegister({ onLogin }) {
   const [email, setEmail] = useState("");
